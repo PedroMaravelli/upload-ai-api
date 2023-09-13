ce5348acbbc58af797414ce56834415666a13b5b
PedroMaravelli <phmaravelli123@gmail.com>
2023-09-13 19:59:36 -0300

    'initial-commit'

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import { prisma} from './lib/prisma'
+import {fastify} from 'fastify'
+import { getAllPrompts } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-video'
+import { createTranscription } from './routes/create-transcription'
+import { generateAiCompletion } from './routes/generate-ai-completion'
+import fastifyCors from '@fastify/cors'
+
+const app = fastify()
+
+app.register(fastifyCors, {
+    origin:'*'
+})
+
+app.register(getAllPrompts)
+app.register(uploadVideoRoute)
+app.register(createTranscription)
+app.register(generateAiCompletion)
+
+app.listen({
+    port: 3333
+}).then(()=> {
+    console.log('Server On!!!');
+    
+})
